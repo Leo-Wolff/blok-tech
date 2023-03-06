5d12c1a9b37838cb5a89b8acc73f31d849a0ed8c
Leo-Wolff <leokramer17@gmail.com>
2023-03-06 13:22:14 +0100

    Add routes + templating
    Handlebars is geïnstalleerd en zijn verbonden met de routes gemaakt in server.js. Voor het geval dat is zijn alle html pagina's verplaatst naar een folder "oud-html", in het geval als deze pagina's later nog een nut zullen hebben.
    
    Dynamische data is nog niet toegevoegd, CSS moet nog gedaan worden.

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express')
+const Handlebars = require("handlebars")
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('view engine', 'handlebars'); // view engine HBS
+app.set('views', 'view');
+
+app
+  .use('/static', express.static('static'))
+  .get('/', onhome) //index.html
+  .get('/letter', onletter) //text-editor.html
+  .get('/bottle', onbottle) //bottle-editor.html
+  .get('/drafts', ondrafts) //drafts.html
+  .get('/ocean', onocean) //bottle-throw-ocean.html
+  .get('/campfire', oncampfire) //bottle-throw-campfire.html
+
+function onhome(req,res) {
+  res.render('index.hbs')
+  res.status(200)
+}
+
+function onletter(req,res) {
+  res.render('text-editor.hbs')
+  res.status(200)
+}
+
+function onbottle(req,res) {
+  res.render('bottle-editor.hbs')
+  res.status(200)
+}
+
+function ondrafts(req,res) {
+  res.render('drafts.hbs')
+  res.status(200)
+}
+
+function onocean(req,res) {
+  res.render('bottle-throw-ocean.hbs')
+  res.status(200)
+}
+
+function oncampfire(req,res) {
+  res.render('bottle-throw-campfire.hbs')
+  res.status(200)
+}
+
+app.get('*', (req, res) => {
+  res.send('Error 404 Not found..')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Wow look at that port ${port}`)
 })
