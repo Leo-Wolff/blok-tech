4ac88efd91841d042be128496edeb81559d63dca
Leo-Wolff <leokramer17@gmail.com>
2023-03-09 16:30:31 +0100

    Create connection to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const Handlebars = require("handlebars")
 const app = express()
@@ -55,3 +56,18 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Wow look at that port ${port}`)
 })
+
+//Database connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASS + '@' + process.env.DB_NAME + '.' + process.env.DB_HOST + '/?retryWrites=true&w=majority';
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
